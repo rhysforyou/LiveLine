25bfc2d74dbdfe819d469bd29205177e9f4f43f6
Rhys Powell <rhys@rpowell.me>
2014-10-08 17:41:28 +1100

    Display dialog if location services disabled

--- LiveLine/Controllers/RecordJourneyViewController.swift
+++ LiveLine/Controllers/RecordJourneyViewController.swift
@@ -24,17 +24,29 @@ class RecordJourneyViewController: UIViewController {
     func checkLocationPermissions() {
         switch (CLLocationManager.authorizationStatus()) {
         case .Denied, .Restricted:
-            print("Location monitoring disabled")
+            displayDialog("Location Monitoring Disabled", message: "LiveLine needs location services to work. Please quit the app and enable them in System Settings → Privacy → Location Services → LiveLine.")
         case .NotDetermined:
             locationManager?.requestAlwaysAuthorization()
+            checkLocationPermissions()
+        case .Authorized:
+            print("Location monitoring enabled always")
             locationManager?.startUpdatingLocation()
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .Follow
-        case .Authorized:
-            print("Location monitoring enabled always")
         case .AuthorizedWhenInUse:
             print("Location monitoring enabled while in-use")
         }
+    }
+    
+    func displayDialog(title: String?, message: String?, dismissable: Bool = true) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        if (dismissable) {
+            let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: { (action: UIAlertAction!) in })
+            alertController.addAction(defaultAction)
+        }
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
