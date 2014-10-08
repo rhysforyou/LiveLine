2f5ea47353d3977af7fbfad69778d4b0584a7c8e
Rhys Powell <rhys@rpowell.me>
2014-10-08 16:12:27 +1100

    Added view controllers for basic app layout

---
+++ LiveLine/Controllers/PastJourneysViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PastJourneysViewController.swift
+//  LiveLine
+//
+//  Created by Rhys Powell on 8/10/2014.
+//  Copyright (c) 2014 Rhys Powell. All rights reserved.
+//
+
+import UIKit
+
+class PastJourneysViewController: UITableViewController {
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillDisappear(animated)
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        // configure the cell
+        
+        return cell
+    }
+}
---
+++ LiveLine/Controllers/RecordJourneyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecordJourneyViewController.swift
+//  LiveLine
+//
+//  Created by Rhys Powell on 8/10/2014.
+//  Copyright (c) 2014 Rhys Powell. All rights reserved.
+//
+
+import UIKit
+
+class RecordJourneyViewController: UIViewController {
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .Default
+    }
+}
